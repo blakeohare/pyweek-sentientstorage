f56ce26abc6072c9abcdf299e7b1abc0a9b29bd3
mspaintmaestro <mspaintmaestro@gmail.com>
2014-10-05 18:01:21 +0000

    These binaries will stabilize after a while so each change isn't a 300k download. This includes a title screen and a pixelized transition. Also wrote a clean up script.

--- game/src/TitleScene.py
+++ game/src/TitleScene.py
@@ -7,10 +7,29 @@ class TitleScene:
 	def update(self, events):
 		self.x += 2
 		self.y += 1
+		action = None
 		for ev in events:
 			if ev.type == 'quit':
 				self.next = None
+			elif ev.type == 'mouseleftdown':
+				x = ev.x
+				y = ev.y
+				if x > 87 and x < 209 and y > 127 and y < 152:
+					action = 'new'
+				elif x > 87 and x < 209 and y > 158 and y < 187:
+					action = 'load'
+				elif x > 115 and x < 164 and y > 191 and y < 216:
+					action = 'exit'
+		
+		if action == 'new':
+			self.next = PixelTransitionScene(self, PlayScene('attic'))
+		elif action == 'load':
+			self.next = LoadGameScene(self)
+		elif action == 'exit':
+			self.next = PixelTransitionScene(self, None)
+		
 	
 	def render(self, screen, images, rc):
-		$screen_fill(screen, 0, 50, 100)
-		$image_blit(screen, images['test'], self.x, self.y)
+		#$screen_fill(screen, 0, 50, 100)
+		$image_blit(screen, images['menus/title'], 0, 0)
+		
