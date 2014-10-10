96a6ddff3988b93b241b850cfdfee1f800481b18
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@40a7f134-84ab-da10-4498-9d4c4e48b5ab>
2014-10-10 02:27:02 +0000

    give the ball and bat to the baseball players for gum

--- trunk/game/src/ApplyItem.py
+++ trunk/game/src/ApplyItem.py
@@ -5,6 +5,64 @@ def apply_item(walky_surface, area, area_id, item, sprite, region_id):
 			if sprite.type == 'mothercar1':
 				if item == 'bluepin':
 					ai_give_pin_to_mother(walky_surface, sprite, area, log)
+	elif area_id == 'sports1':
+		if sprite != None:
+			if sprite.type  == 'bballplayer1' or sprite.type == 'bballplayer2' or sprite.type == 'bballplayer3':
+				if item == 'ball' or item == 'bat':
+					has_both = log.get_int('HAS_BAT',0) == 2
+					ai_give_ball_to_player(walky_surface, sprite, area, log, item, has_both)
+				elif log.get_int('HAS_WRAPPEDGUM', 0) != 0:
+					if item == 'house' or item == 'volcanopog' or item == 'legopog' or item == 'trainpog':
+						ai_give_base_to_player(walky_surface, sprite, area, log, item)
+
+def ai_give_ball_to_player(walky_surface, sprite, area, log, item, has_both):
+	log.set_int('HAS_' + $string_upper(item), 2)
+	if has_both:
+		walky_surface.invoke_dialog([
+				"Oh, look, a " + item + "!",
+				"Now we can actually play some ball!",
+				"Have some gum for your troubles."
+				], None, None)
+		log.set_int('HAS_WRAPPEDGUM', 1)
+	else:
+		if item == 'ball':
+			walky_surface.invoke_dialog([
+				"Oh, look, a ball!",
+				"Now we just need a bat."], None, None)
+		else:
+			walky_surface.invoke_dialog([
+				"Oh, look, a bat!",
+				"Now we just need a ball."], None, None)
+		
+			
+
+def ai_give_base_to_player(walky_surface, sprite, area, log, item):
+	log.set_int('HAS_' + $string_upper(item), 2)
+	
+	sum = log.get_int('HAS_HOUSE', 0) + log.get_int('HAS_TRAINPOG', 0) + log.get_int('HAS_LEGOPOG', 0) + log.get_int('HAS_VOLCANOPOG', 0)
+	if sum == 8:
+		walky_surface.invoke_dialog([
+			"Hey, we have all the bases now!",
+			"Take this as a token of our ",
+			"appreciation"
+			], None, None)
+	else:
+		walk_surface.invoke_dialog([
+			"Thanks! This will make seeing the",
+			"bases easier.",
+			"",
+			"(He gives you a photo piece)"
+		], None, None)
+		
+
+def ai_give_ball_and_bat_to_player(walky_surface, sprite, area, log):
+	log.set_int('HAS_BALL', 2)
+	log.set_int('HAS_BAT', 2)
+	walky_surface.invoke_dialog([
+		"Hooray!",
+		"Now we can play.",
+		"Have some buble gum for your troubles.",
+		"Now if we only had some bases."], None, None)
 
 def ai_give_pin_to_mother(walky_surface, sprite, area, log):
 	sprite.dead = True
--- trunk/game/src/InventoryOverlay.py
+++ trunk/game/src/InventoryOverlay.py
@@ -14,6 +14,7 @@ ALL_ITEMS = [
 		'rubberband',
 		'scottie',
 		'thimble',
+		'trainpog',
 		'trainwheel',
 		'volcanopog',
 		'wheelbarrow',
