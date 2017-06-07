b06f44603a36d7220afaf53a503ee0c8b8bb8db8
Blake O'Hare <mspaintmaestro@gmail.com>
2017-06-07 21:40:20 +0900

    Initial commit to update to modern Crayon.

--- clean.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-rmdir %PYWEEK19_ROOT%\build /s /q
-mkdir %PYWEEK19_ROOT%\build
-rmdir %PYWEEK19_ROOT%\js_local /s /q
-mkdir %PYWEEK19_ROOT%\js_local
-rmdir %PYWEEK19_ROOT%\js_remote /s /q
-mkdir %PYWEEK19_ROOT%\js_remote
-
